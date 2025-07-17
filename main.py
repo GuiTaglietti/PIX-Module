f8b5233d950fc44b000e8a6005073b3334a29a89
ganassini <guiganassini17@gmail.com>
2025-07-17 02:31:28 -0300

    [Pix]: PIX api is now a module (calling in main.py for testing purposes)

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from pix.Pix import Pix
+import argparse
+from sys import argv
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                description="EFI Bank Pix API Wrapper"
+            )
+    parser.add_argument("-a", "--amount", help="specifies the value to be paid")
+    parser.add_argument("-c", "--create-charge", help="create a new charge of AMOUNT", action='store_true')
+    parser.add_argument("-l", "--list-charges", help="list last charges", action='store_true')
+    parser.add_argument("-x", "--txid", help="consult charge by txid")
+    # TODO: inicio/fim args
+    parser.add_argument("-t", "--test-args", help="list provided arguments", action='store_true')
+    args = parser.parse_args()
+    
+    if len(argv) == 1:
+        parser.print_help()
+        exit(1)
+
+    if args.test_args:
+        print(args)
+        exit(0)
+    
+    if args.create_charge:
+        if not args.amount:
+            parser.print_help()
+            exit(1)
+    
+    return args
+
+def main():
+    args = parse_args()
+    pix = Pix()
+
+    if args.list_charges:
+        response = pix.list_charges(inicio="2025-07-16T00:00:00Z", fim="2025-07-17T00:00:00Z")
+        print(response)
+        return
+    elif args.txid:
+        response = pix.detail_single_charge(args.txid)
+        print(response)
+        return
+    else:
+        response = pix.make_cob(args.amount)
+        print(response)
+        return
+    
+
+if  __name__ == "__main__":
+    main()
